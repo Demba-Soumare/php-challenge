cf8798379e38e9813930d6972c4aa445bac253eb
demba <demba08hotmail.fr>
2024-04-02 12:45:32 +0200

    challenge3

---
+++ challenge3.php
@@ -0,0 +1,10 @@
+<?php
+
+$bookTitle=[
+    ['1883', 'ile au tresor'],['1886','enleve'],['1991','the swing']
+];
+foreach ($bookTitle as $book) 
+{echo ">  " . $book[0] . ", " . $book[1] . "<br>";}
+
+
+?>
