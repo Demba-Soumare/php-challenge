155bbc9ac4e7c94e6632bd36a22114ca8a8fe60d
demba <demba08hotmail.fr>
2024-04-04 12:32:33 +0200

    challenge7

---
+++ Challenge 7/index.php
@@ -0,0 +1,34 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    
+    "Demba" => [
+    "City" => "Epinay",
+    "weapon" => "Katana"
+]
+
+];
+
+echo "<pre>";
+var_dump($characters);
+echo "</pre>";
+
+?>
+
+
+
+
+
+
